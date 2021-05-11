7cd4a999706da26b83a524143004b7e1bd9c4b74
Kirill <kirillaromad@mail.ru>
2021-05-11 20:27:48 +0300

    bad code

---
+++ DotNetTest/DotNetTest/ExampleCollection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace DotNetTest
+{
+    public class ExampleCollection
+    {
+        private readonly List<string> _collection = new List<string>();
+
+        public int Length => _collection.Count;
+        
+        public List<string> GetItems()
+        {
+            return _collection;
+        }
+        
+        public void AddItem(string item)
+        {
+            this._collection.Add(item);
+        }
+
+        public string this[int index]
+        {
+            get => _collection[index];
+            set => _collection[index] = value;
+        }
+    }
+}
---
+++ DotNetTest/DotNetTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DotNetTest
+{
+    class Program
+    {
+        public static ExampleCollection AlphabeticalOrderSort(ExampleCollection collection)
+        {
+            string temp;
+            for (var i = 0; i < collection.Length; i++)
+            {
+                for (var j = i + 1; j < collection.Length; j++)
+                {
+                    if (collection[i].ToCharArray()[0] > collection[j].ToCharArray()[0])
+                    {
+                        temp = collection[i];
+                        collection[i] = collection[j];
+                        collection[j] = temp;
+                    }
+                }
+            }
+
+            return collection;
+        }
+        
+        static void Main(string[] args)
+        {
+            var collection = new ExampleCollection();
+            collection.AddItem("foo");
+            collection.AddItem("bar");
+            collection.AddItem("a");
+
+            var sorted = AlphabeticalOrderSort(collection);
+            
+        }
+    }
+}
